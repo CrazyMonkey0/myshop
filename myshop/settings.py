7fa80486ce62bb9d153bf0c114a8ca68bed3b477
CrazyMonkey0 <hubcio1765@gmail.com>
2023-09-27 15:39:08 +0200

    Creating directory templates

--- myshop/settings.py
+++ myshop/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuration for storing images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
