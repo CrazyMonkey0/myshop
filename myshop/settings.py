bde52866fcf5eb60737f975e6c859b6bfdfef8e8
CrazyMonkey0 <hubcio1765@gmail.com>
2023-10-03 00:16:40 +0200

    Adding asynchronous tasks to your application

--- myshop/settings.py
+++ myshop/settings.py
@@ -143,3 +143,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Key to store shopping cart in user session
 CART_SESSION_ID = 'cart'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
